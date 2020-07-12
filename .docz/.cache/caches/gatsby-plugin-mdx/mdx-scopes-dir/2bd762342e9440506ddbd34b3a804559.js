7190666f316c9c15f8a6f34da7d1f55efb441c60
Kelly Mears <developers@tinypixel.dev>
2020-07-12 05:22:41 -0700

    document: useSelection

---
+++ .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/2bd762342e9440506ddbd34b3a804559.js
@@ -0,0 +1,7 @@
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import * as React from 'react';
+export default {
+  Highlight,
+  defaultProps,
+  React
+};
--- .docz/gatsby-config.js
+++ .docz/gatsby-config.js
@@ -62,7 +62,7 @@ const config = {
         title: '@tinypixelco/wp-react-hooks',
         description: 'My awesome app using docz',
         host: 'localhost',
-        port: 3000,
+        port: 3002,
         p: 3000,
         separator: '-',
         paths: {
